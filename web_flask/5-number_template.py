9099c0ee3183ddb181c018a3e88d6e26a3f7daf8
MelissaAT <100169473+MelissaAT@users.noreply.github.com>
2023-04-03 14:05:48 -0400

    4-number_route.py

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -35,7 +35,7 @@ def python_text(text):
 @app.route("/number/<n>", strict_slashes=False)
 def n_text(n):
     if n == int:
-        return f'"{n} is a number"'
+        return f'{n} is a number'
 
 
 if __name__ == '__main__':
