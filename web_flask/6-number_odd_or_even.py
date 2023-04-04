c210fdf3437d443e43e9b0c543c3bed09a081eba
MelissaAT <100169473+MelissaAT@users.noreply.github.com>
2023-04-04 11:02:16 -0400

    6

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -44,8 +44,13 @@ def number_template(n):
 
 
 @app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
-def number_template1(n):
-    return render_template('6-number_odd_or_even.html', number=n)
+def number_template1(n=None):
+    a = ""
+    if n % 2 == 0:
+        a = "even" 
+    else:
+        a = "odd"
+    return render_template('6-number_odd_or_even.html', number=n, a=a)
 
 
 if __name__ == '__main__':
