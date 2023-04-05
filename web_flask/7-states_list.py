58006d62b9b4095277f3dee9e8e605bf60aa432e
MelissaAT <100169473+MelissaAT@users.noreply.github.com>
2023-04-05 13:31:59 -0400

    7

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -4,14 +4,16 @@ Flask web application"""
 
 from flask import Flask, render_template
 from models import storage
+from models.state import State
 
 
 app = Flask(__name__)
 
 @app.route("/states_list", strict_slashes=False)
-def s_lists():
-    states = storage.all()
-    return render_template('7-states_list.html', states=states)
+def states_list():
+    states = storage.all(State).values()
+    sorted_states = sorted(states, key=lambda state: state.name)
+    return render_template('states_list.html', states=sorted_states)
 
 @app.teardown_appcontext
 def s_close():
