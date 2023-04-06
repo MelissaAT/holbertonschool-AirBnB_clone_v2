871e6121a39066dc24cf2cea13628b4cf36830cd
MelissaAT <100169473+MelissaAT@users.noreply.github.com>
2023-04-06 10:29:45 -0400

    .

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -9,6 +9,51 @@ from models.state import State
 
 app = Flask(__name__)
 
+@app.route("/", strict_slashes=False)
+def display_text():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def display_hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def c_text(text):
+    return "C %s" % text.replace('_', ' ')
+
+
+@app.route("/python/", strict_slashes=False, defaults={'text': 'is cool'})
+@app.route("/python/<text>", strict_slashes=False)
+def python_text(text):
+    if text == "is cool":
+        return "Python is cool"
+    else:
+        return "Python " + f'{text}'.replace("_", " ")
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def n_text(n):
+    if type(n) == int:
+        return f'{n} is a number'
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def number_template(n):
+    return render_template('5-number.html', number=n)
+
+
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
+def number_template1(n=None):
+    a = ""
+    if n % 2 == 0:
+        a = "even"
+    else:
+        a = "odd"
+    return render_template('6-number_odd_or_even.html', number=n, a=a)
+
+
 
 @app.route('/states_list', strict_slashes=False)
 def hello_8():
--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -8,6 +8,61 @@ from models.city import City
 
 app = Flask(__name__)
 
+@app.route("/", strict_slashes=False)
+def display_text():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def display_hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def c_text(text):
+    return "C %s" % text.replace('_', ' ')
+
+
+@app.route("/python/", strict_slashes=False, defaults={'text': 'is cool'})
+@app.route("/python/<text>", strict_slashes=False)
+def python_text(text):
+    if text == "is cool":
+        return "Python is cool"
+    else:
+        return "Python " + f'{text}'.replace("_", " ")
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def n_text(n):
+    if type(n) == int:
+        return f'{n} is a number'
+
+
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def number_template(n):
+    return render_template('5-number.html', number=n)
+
+
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
+def number_template1(n=None):
+    a = ""
+    if n % 2 == 0:
+        a = "even"
+    else:
+        a = "odd"
+    return render_template('6-number_odd_or_even.html', number=n, a=a)
+
+
+@app.route('/states_list', strict_slashes=False)
+def hello_8():
+    states_dict = storage.all(State)
+    return render_template('7-states_list.html', states=states_dict.values())
+
+
+@app.teardown_appcontext
+def s_close(exception):
+    storage.close()
+
 
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
@@ -23,4 +78,4 @@ def close_storage(exception):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5001)
