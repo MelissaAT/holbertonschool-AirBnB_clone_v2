ed1f79ada109004cbe3395b3e7c4b3642c6e38ba
Melissa Arroyo <melissaarroyotorres9806@gmail.com>
2023-04-06 11:35:57 -0400

    linux

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -67,4 +67,4 @@ def s_close(exception):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001)
+    app.run(host='0.0.0.0', port=5000)
--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -78,4 +78,4 @@ def close_storage(exception):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001)
+    app.run(host='0.0.0.0', port=5000)
