4409d69afe8df3f8e45febb0ea352e457afa2b22
MelissaAT <100169473+MelissaAT@users.noreply.github.com>
2023-04-07 08:45:15 -0400

    11

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -55,15 +55,24 @@ def number_template1(n=None):
 
 
 
-@app.route('/states_list', strict_slashes=False)
-def hello_8():
-    states_dict = storage.all(State)
-    return render_template('7-states_list.html', states=states_dict.values())
+from flask import Flask, render_template
+from models import storage
 
+app = Flask(__name__)
 
 @app.teardown_appcontext
-def s_close(exception):
+def close_session(exception=None):
     storage.close()
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    states = storage.all('State').values()
+    sorted_states = sorted(states, key=lambda state: state.name)
+    return render_template('7-states_list.html', states=sorted_states)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
 
 
 if __name__ == '__main__':
